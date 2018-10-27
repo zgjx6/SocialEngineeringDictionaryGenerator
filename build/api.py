61336d3937a5eb3120e0c5556cef3d3ae936b0eb
zgjx <953870958@qq.com>
2018-10-27 23:41:02 +0800

    update encrypt strategy

--- build/api.py
+++ build/api.py
@@ -6,16 +6,15 @@ Social Engineering Dictionary Generator
 初步设想为：根据常见的个人信息，分别生成少量、中量、大量密码，以应付不同的需求。
 '''
 
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 
-import string
 import re
 import itertools
 import json
 import sys
 import os
 
-app = Flask(__name__,template_folder='')
+app = Flask(__name__, template_folder='')
 dirname = sys.path[0]
 
 
@@ -28,20 +27,12 @@ def getLowAndUpStr(s):  # 得到字符串大小写，小于三位自动重复
     return temp
 
 
-def distinctList(l):  # 列表去重,去空
-    r = []
-    for i in l:
-        if i and i not in r:
-            r.append(i)
-    return r
+def distinctList(l):  # 列表去重
+    return list(set(l))
 
 
 def dropHeadTail(l, start=6, end=16):  # 列表去掉过长和过短
-    r = []
-    for i in l:
-        if start <= len(i) <= end:
-            r.append(i)
-    return r
+    return [x for x in l if start <= len(x) <= end]
 
 
 def dropStingInt(l, rtype):  # 去掉字母或数字
@@ -57,6 +48,8 @@ def dropStingInt(l, rtype):  # 去掉字母或数字
             r.append(i)
     return r
 
+
+# SPA，单页面应用
 @app.route('/',defaults={'path':''})
 @app.route('/<path:path>')
 def catch_all(path):
@@ -75,7 +68,6 @@ def index_get():
     for i in [firstName[0:1] + lastNameAB, firstNameCombine[0] + lastNameAB, lastNameAB + firstName[0:1], lastNameAB + firstNameCombine[0], firstName[0:1] + ''.join(lastName)]:
         name_all += [i]
     name_all += [''.join(lastName), firstName]
-    # print('name_all',name_all)
     # birthday
     birthday = data.get('birthday', '')
     birthday2 = data.get('birthday2', '')
@@ -110,18 +102,16 @@ def index_get():
             phone_all.append(i[-6:])
             phone_all.append(i[-4:])
             phone_all.append(i[-3:])
-    # print('phone_all',phone_all)
     # other
     userName = data.get('userName', '')
     account = data.get('account', '')
     qq = data.get('qq', '') if re.match('^[0-9]{5,13}$', data.get('qq', '')) != None else ''
-    # url=data.get('telephone', '')
     organization = data.get('organization', '') if re.match(
         '^[0-9a-zA-Z@./ -]*$', data.get('organization', '')) != None else ''
     company = data.get('company', '') if re.match('^[0-9a-zA-Z@./ -]*$', data.get('company', '')) != None else ''
-    password = data.get('password', '')
+    # password = data.get('password', '')
     # password1=data.get('password1', '')
-    password2 = data.get('password2', '')
+    # password2 = data.get('password2', '')
     likeuse = data.get('likeuse', '')
     other = ''
     other_all = [userName, account, qq, organization, company, likeuse, other]
@@ -139,7 +129,6 @@ def index_get():
             idcard_all.append(idcard[-7:-1])
             idcard_all.append(idcard[-5:-1])
             idcard_all.append(idcard[-4:-1])
-    # print('idcard_all',idcard_all)
     # workNo
     workNo = data.get('workNo', '') if re.match('^[0-9a-zA-Z@./ -]*$', data.get('workNo', '')) != None else ''
     workNo_all = [workNo]
@@ -152,15 +141,14 @@ def index_get():
             workNo_all.append(workNo[-6:])
         if len(workNo) >= 8:
             workNo_all.append(workNo[-8:])
-    # print('workNo_all',workNo_all)
     # second person
-    metaFirstName = data.get('firstName2', '')
-    mateLastName = [data.get('secondName2', ''), data.get('thirdName2', '')]
-    metaBirthday = data.get('birthday3', '')
-    metaBirthday2 = data.get('birthday4', '')
+    # metaFirstName = data.get('firstName2', '')
+    # mateLastName = [data.get('secondName2', ''), data.get('thirdName2', '')]
+    # metaBirthday = data.get('birthday3', '')
+    # metaBirthday2 = data.get('birthday4', '')
     connector = data.get('connector', '')
     common = list(map(str, range(10))) + ['a', 'z', 'q'] + ['11', '12', '01', 'qq', 'aa', 'zz', '00', '66', '88', '99',
-                                                            'ab', 'zx', 'az', 'qw', 'qa'] + ['123', '888', '666', '000', '111', 'aaa', 'abc', 'qaz', 'qwe', 'asd', 'zxc']
+                                                            'ab', 'zx', 'az', 'qw', 'qa'] + ['123', '888', '666', '000', '111', 'aaa', 'abc', 'qaz', 'qwe', 'asd', 'zxc'] + ['1234','1qaz','qwer','asdf','zxcv','1357','2468','0123','6789']+['12345','123456']
     # generate password
     pass_list_all = []
     for i in [name_all, birthday_all, phone_all, idcard_all, workNo_all, other_all]:
@@ -171,57 +159,60 @@ def index_get():
     pass_list_all.append(email_all)
     pass_list_all.append(common)
     pass_first_all = itertools.chain(*pass_list_all)
-    pass_first_all = distinctList(pass_first_all)
+
     numberFilter = data.get('numberFilter', '')
     stringFilter = data.get('stringFilter', '')
     shortFilter = data.get('shortFilter', '')
     longFilter = data.get('longFilter', '')
     short = data.get('short', '')
     _long = data.get('long', '')
-    # print(numberFilter,stringFilter,shortFilter,longFilter,short,_long)
-    if shortFilter == True:
-        short = int(short) if int(short) > 0 else 1
-    else:
-        short = 1
-    if longFilter == True:
-        _long = int(_long) if int(_long) > 0 else 100
-    else:
-        _long = 100
-    pass_first_all = dropHeadTail(pass_first_all, start=short, end=_long)
-    if numberFilter == True:
-        pass_first_all = dropStingInt(pass_first_all, 'int')
-    if stringFilter == True:
-        pass_first_all = dropStingInt(pass_first_all, 'str')
-    pass_second_all = []
+    # 开始生成二阶和三阶密码
+    pass_second_all, pass_third_all = [], []
     for i, j in enumerate(pass_list_all):
         for k in pass_list_all[i + 1:]:
-            pass_second_all += [''.join(m) for m in list(itertools.product(j, k))] + [''.join(m)
-                                                                                      for m in list(itertools.product(k, j))]
+            pass_second_all += [''.join(m) for m in itertools.product(j, k)] + [''.join(m) for m in itertools.product(k, j)]
+            pass_third_all += [''.join(m) for m in itertools.product(j, connector, k)] + [''.join(m) for m in itertools.product(k, connector, j)]
+    short = int(short) if shortFilter == True and int(short) > 0 else 1    # 最小长度
+    _long = int(_long) if longFilter == True and int(_long) > 0 else 100    # 最大长度
+
+    pass_first_all = distinctList(pass_first_all)
+    pass_first_all = dropHeadTail(pass_first_all, start=short, end=_long)
     pass_second_all = distinctList(pass_second_all)
     pass_second_all = dropHeadTail(pass_second_all, start=short, end=_long)
+    pass_third_all = distinctList(pass_third_all)
+    pass_third_all = dropHeadTail(pass_third_all, start=short, end=_long)
     if numberFilter == True:
+        pass_first_all = dropStingInt(pass_first_all, 'int')
         pass_second_all = dropStingInt(pass_second_all, 'int')
+        pass_third_all = dropStingInt(pass_third_all, 'int')
     if stringFilter == True:
+        pass_first_all = dropStingInt(pass_first_all, 'str')
         pass_second_all = dropStingInt(pass_second_all, 'str')
-    # print(len(pass_first_all))
-    # print(len(pass_second_all))
-    return jsonify({"pass_first_all": '\n'.join(pass_first_all), "pass_second_all": '\n'.join(pass_second_all)})
+        pass_third_all = dropStingInt(pass_third_all, 'str')
+
+    return jsonify({"pass_first_all": '\n'.join(pass_first_all), "pass_second_all": '\n'.join(pass_second_all), "pass_third_all": '\n'.join(pass_third_all)})
 
 
 @app.route('/api/getCommon', methods=['post'])
-def common():
+def getCommon():
     with open(os.path.join(dirname, 'static', 'file', 'mypass100.txt'), 'r') as f:
         content100 = f.read()
     with open(os.path.join(dirname, 'static', 'file', 'csdn_1700.txt'), 'r') as f:
         content1700 = f.read()
-    return jsonify({'content100':content100,'content1700':content1700})
+    with open(os.path.join(dirname, 'static', 'file', '10000pass.txt'), 'r') as f:
+        content10000 = f.read()
+    return jsonify({'content100':content100,'content1700':content1700,'content10000':content10000})
+
+
 if __name__ == "__main__":
     from flask_compress import Compress
     Compress(app)
     app.run(debug=True)
 
 
-# 二阶密码优化
-# 第二人
-# 三阶
-# 密码数量显示
+# TODO:网址
+# TODO:旧密码
+# TODO:第二人名字
+# TODO:Sanic,全异步
+
+
